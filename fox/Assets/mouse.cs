ceaad51ca83f87952a62aacf5f8512c3bf780c56
hank634 <55218499+hank634@users.noreply.github.com>
2019-12-26 12:14:19 +0800

    老鼠

---
+++ fox/Assets/mouse.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float speed = 1.5f;
+    [Header("傷害"), Range(0, 100)]
+    public float damage = 20;
+
+    public Transform checkPoint;
+
+    private Rigidbody2D r2d;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;//圖示 顏色=顏色 黃色
+        Gizmos.DrawRay(checkPoint.position, -checkPoint.up * 1.5f); //圖示 繪製射線(中心點 方向*長度)
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name=="FOX")
+        {
+            Track(collision.transform.position);
+        }
+    }
+
+
+
+    /// <summary>
+    /// 隨機移動
+    /// </summary>
+    private void Move ()
+    {
+       // r2d.AddForce(new Vector2(-speed, 0));//世界座標
+        r2d.AddForce(-transform.right*speed);//區域座標 2D transform.right 右邊 tranform.up 上方
+
+        //物理 射線碰撞
+        RaycastHit2D hit= Physics2D.Raycast(checkPoint.position, -checkPoint.up, 1.5f, 1 << 8);
+        if(hit==false)
+        {
+            transform.eulerAngles += new Vector3(0, 180, 0);
+        }
+    }
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track(Vector3 target)
+    {
+       if(target.x<transform.position.x)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+       else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+}
